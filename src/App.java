4e584a764009f0fe88309d88b6f0338ec2800ea4
alejacorrea <alejandracp63@gmail.com>
2025-05-08 14:59:53 -0500

    Se crea repositorio

---
+++ src/App.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingresa nombre: ");
+        String nombreUsuario = sc.next();
+        System.out.println("Ingresa clave: ");
+        String claveUsuario = sc.next();
+
+        if (nombreUsuario.equals("admin") && claveUsuario.equals("clave123")) {
+            System.out.println("Bienvenido");
+        }else{
+            System.out.println("Datos incorrectos");
+        }
+        
+    }
+}
